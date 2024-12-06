08a21369cf7944eed30debf68c47fd6b9830c395
Billy.Zheng <vil963@gmail.com>
2024-12-06 14:45:50 +0800

    Add git hash as rev version

--- src/sentry.cr
+++ src/sentry.cr
@@ -5,5 +5,10 @@ require "./sentry/sound_file_storage"
 require "./sentry/process_runner.cr"
 
 module Sentry
-  VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+  VERSION = {{
+              `shards version "#{__DIR__}"`.chomp.stringify +
+              " (rev " +
+              `git rev-parse --short HEAD`.chomp.stringify +
+              ")"
+            }}
 end
