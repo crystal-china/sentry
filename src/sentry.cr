02dab34542675704e63124628c73feb142f15e37
Billy.Zheng <vil963@gmail.com>
2025-03-19 01:59:28 +0800

    Add date into VERSION

--- src/sentry.cr
+++ src/sentry.cr
@@ -8,6 +8,7 @@ module Sentry
               `shards version "#{__DIR__}"`.chomp.stringify +
               " (rev " +
               `git rev-parse --short HEAD`.chomp.stringify +
-              ")"
+              ")" +
+              `date '+ %Y-%m-%d %H:%M:%S'`.chomp.stringify
             }}
 end
